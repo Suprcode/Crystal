8016bfada766050d6626c387d35a8983bbf68e29
https://github.com/Duane01/mir2 <duaneg99@hotmail.co.uk>
2015-10-03 19:24:49 +0200

    Gem Independence

--- Common.cs
+++ Common.cs
@@ -56,7 +56,33 @@ public enum ItemGrade : byte
     Legendary = 3,
     Mythical = 4,
 }
-
+public enum StatType : byte
+{
+    AC = 0,
+    MAC = 1,
+    DC = 2,
+    MC = 3,
+    SC = 4,
+    HP = 5,
+    MP = 6,
+    HP_Precent = 7,
+    MP_Precent = 8,
+    HP_Regen = 9,
+    MP_Regen = 10,
+    ASpeed = 11,
+    Luck = 12,
+    Strong = 13,
+    Accuracy = 14,
+    Agility = 15,
+    MagicResist = 16,
+    PoisonResist = 17,
+    PoisonAttack = 18,
+    PoisonRegen = 19,
+    Freezing = 20,
+    Holy = 21,
+    Durability = 22,
+    Unknown = 23
+}
 public enum RefinedValue : byte
 {
     None = 0,
