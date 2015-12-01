b903b0341c70eeeb4b31c6228a56e505268a0490
Elijah <ash@audis.co.uk>
2015-12-01 16:42:30 +0000

    Add Conquest that i missed.

---
+++ Server/MirObjects/ConquestGateObject.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Server.MirEnvir;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirObjects.Monsters;
+
+namespace Server.MirObjects
+{
+
+    public class ConquestGateObject
+    {
+        protected static Envir Envir
+        {
+            get { return SMain.Envir; }
+        }
+
+        public int Index;
+        public uint Health;
+
+        public ConquestGateInfo Info;
+
+        public ConquestObject Conquest;
+
+        public SabukGate Gate;
+
+
+        public ConquestGateObject()
+        {
+
+        }
+        public ConquestGateObject(PlayerObject owner, string name)
+        {
+
+        }
+        public ConquestGateObject(BinaryReader reader)
+        {
+            Index = reader.ReadInt32();
+            Health = reader.ReadUInt32();
+        }
+        public void Save(BinaryWriter writer)
+        {
+            if (Gate != null) Health = Gate.HP;
+            writer.Write(Index);
+            writer.Write(Health);
+        }
+
+
+        public void Spawn()
+        {
+            if (Gate != null) Gate.Despawn();
+
+            MonsterInfo monsterInfo = Envir.GetMonsterInfo(Info.MobIndex);
+
+            if (monsterInfo == null) return;
+            if (monsterInfo.AI != 72) return;
+
+            Gate = (SabukGate)MonsterObject.GetMonster(monsterInfo);
+
+            if (Gate == null) return;
+
+            Gate.Conquest = Conquest;
+            Gate.GateIndex = Index;
+
+            Gate.Spawn(Conquest.ConquestMap, Info.Location);
+
+            if (Health == 0)
+                Gate.Die();
+            else
+                Gate.SetHP(Health);
+
+            Gate.CheckDirection();
+        }
+
+        public uint GetRepairCost()
+        {
+            uint cost = 0;
+
+            if (Gate != null)
+            {
+                if (Info.RepairCost != 0 && Gate.MaxHP != Gate.HP)
+                cost = Info.RepairCost / (Gate.MaxHP / (Gate.MaxHP - Gate.HP));
+            }
+            return cost;
+        }
+
+        public void Repair()
+        {
+            if (Gate == null)
+            {
+                Spawn();
+                return;
+            }
+
+            if (Gate.Dead) Gate.Revive(Gate.MaxHP, false);
+            else
+                Gate.HP = Gate.MaxHP;
+
+            Gate.CheckDirection();
+          
+
+        }
+    }
+}
---
+++ Server/MirObjects/Monsters/Wall.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Server.MirObjects.Monsters
+{
+    public class Wall : MonsterObject
+    {
+        public ConquestObject Conquest;
+        public int WallIndex;
+
+        protected internal Wall(MonsterInfo info) : base(info)
+        {
+            Direction = MirDirection.Up;
+        }
+        public override void Despawn()
+        {
+            base.Despawn();
+        }
+        protected override bool CanAttack
+        {
+            get
+            {
+                return false;
+            }
+        }
+        protected override bool CanMove { get { return false; } }
+        public override void Turn(MirDirection dir) { }
+
+        public override bool Walk(MirDirection dir) { return false; }
+
+        public override int Attacked(PlayerObject attacker, int damage, DefenceType type = DefenceType.ACAgility, bool damageWeapon = true)
+        {
+            CheckDirection();
+
+            if (!Conquest.WarIsOn || attacker.MyGuild != null && Conquest.Owner == attacker.MyGuild.Guildindex) damage = 0;
+
+            return base.Attacked(attacker, damage, type, damageWeapon);
+        }
+
+        public override int Attacked(MonsterObject attacker, int damage, DefenceType type = DefenceType.ACAgility)
+        {
+            CheckDirection();
+
+            if (!Conquest.WarIsOn) damage = 0;
+
+            return base.Attacked(attacker, damage, type);
+        }
+
+        public void RepairWall()
+        {
+            if (HP == 0)
+                Revive(MaxHP, false);
+            else
+                SetHP(MaxHP);
+
+            CheckDirection();
+        }
+
+        public void CheckDirection()
+        {
+            MirDirection newDirection = (MirDirection)(4 - GetDamageLevel());
+
+            if (newDirection != Direction)
+            {
+                Direction = newDirection;
+                Broadcast(new S.ObjectTurn { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation });
+            }
+        }
+
+        public override void Die()
+        {
+            base.Die();
+        }
+
+        protected int GetDamageLevel()
+        {
+            int level = (int)Math.Round((double)(4 * HP) / MaxHP);
+
+            if (level < 1) level = 1;
+
+            return level;
+        }
+
+        protected override bool CanRegen
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
+
