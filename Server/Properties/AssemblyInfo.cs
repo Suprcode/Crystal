fd251652d53607804d201ddb1983c8f50492722e
suprcode <development@suprcode.com>
2016-11-05 10:00:52 +0000

    .

--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("2016.10.16.2207")]
-[assembly: AssemblyVersion("2016.10.16.2207")]
+[assembly: AssemblyFileVersion("2016.11.5.1000")]
+[assembly: AssemblyVersion("2016.11.5.1000")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
