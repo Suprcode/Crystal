8a67320d671d0ff3ae72110f905b9ceac114b20e
Sener <765335321@qq.com>
2019-03-21 20:26:03 +0800

    更新 Auto Patcher Admin 中文本地化

--- AutoPatcherAdmin/AMain.Designer.cs
+++ AutoPatcherAdmin/AMain.Designer.cs
@@ -54,73 +54,73 @@
             // 
             // ClientTextBox
             // 
-            this.ClientTextBox.Location = new System.Drawing.Point(99, 12);
+            this.ClientTextBox.Location = new System.Drawing.Point(99, 11);
             this.ClientTextBox.Name = "ClientTextBox";
-            this.ClientTextBox.Size = new System.Drawing.Size(254, 20);
+            this.ClientTextBox.Size = new System.Drawing.Size(254, 21);
             this.ClientTextBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(77, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Client Directory:";
+            this.label1.Text = "Client 目录:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 41);
+            this.label2.Location = new System.Drawing.Point(30, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(59, 12);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Host Address:";
+            this.label2.Text = "FTP 地址:";
             // 
             // HostTextBox
             // 
-            this.HostTextBox.Location = new System.Drawing.Point(99, 38);
+            this.HostTextBox.Location = new System.Drawing.Point(99, 35);
             this.HostTextBox.Name = "HostTextBox";
-            this.HostTextBox.Size = new System.Drawing.Size(254, 20);
+            this.HostTextBox.Size = new System.Drawing.Size(254, 21);
             this.HostTextBox.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(57, 67);
+            this.label3.Location = new System.Drawing.Point(54, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.Size = new System.Drawing.Size(35, 12);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Login:";
+            this.label3.Text = "用户:";
             // 
             // LoginTextBox
             // 
-            this.LoginTextBox.Location = new System.Drawing.Point(99, 64);
+            this.LoginTextBox.Location = new System.Drawing.Point(99, 59);
             this.LoginTextBox.Name = "LoginTextBox";
-            this.LoginTextBox.Size = new System.Drawing.Size(254, 20);
+            this.LoginTextBox.Size = new System.Drawing.Size(254, 21);
             this.LoginTextBox.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(37, 93);
+            this.label4.Location = new System.Drawing.Point(54, 86);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.Size = new System.Drawing.Size(35, 12);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Password:";
+            this.label4.Text = "密码:";
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(99, 90);
+            this.PasswordTextBox.Location = new System.Drawing.Point(99, 83);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(254, 20);
+            this.PasswordTextBox.Size = new System.Drawing.Size(254, 21);
             this.PasswordTextBox.TabIndex = 7;
             // 
             // ProcessButton
             // 
-            this.ProcessButton.Location = new System.Drawing.Point(99, 139);
+            this.ProcessButton.Location = new System.Drawing.Point(99, 128);
             this.ProcessButton.Name = "ProcessButton";
-            this.ProcessButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcessButton.Size = new System.Drawing.Size(75, 21);
             this.ProcessButton.TabIndex = 9;
             this.ProcessButton.Text = "Process";
             this.ProcessButton.UseVisualStyleBackColor = true;
@@ -128,106 +128,106 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 182);
+            this.progressBar1.Location = new System.Drawing.Point(12, 168);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(372, 16);
+            this.progressBar1.Size = new System.Drawing.Size(372, 15);
             this.progressBar1.TabIndex = 12;
             // 
             // ActionLabel
             // 
             this.ActionLabel.AutoSize = true;
-            this.ActionLabel.Location = new System.Drawing.Point(59, 166);
+            this.ActionLabel.Location = new System.Drawing.Point(56, 153);
             this.ActionLabel.Name = "ActionLabel";
-            this.ActionLabel.Size = new System.Drawing.Size(24, 13);
+            this.ActionLabel.Size = new System.Drawing.Size(29, 12);
             this.ActionLabel.TabIndex = 11;
             this.ActionLabel.Text = "Idle";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 166);
+            this.label5.Location = new System.Drawing.Point(12, 153);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(47, 12);
             this.label5.TabIndex = 10;
             this.label5.Text = "Action:";
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(12, 251);
+            this.progressBar2.Location = new System.Drawing.Point(12, 232);
             this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(372, 10);
+            this.progressBar2.Size = new System.Drawing.Size(372, 9);
             this.progressBar2.TabIndex = 13;
             // 
             // SpeedLabel
             // 
             this.SpeedLabel.AutoSize = true;
-            this.SpeedLabel.Location = new System.Drawing.Point(59, 235);
+            this.SpeedLabel.Location = new System.Drawing.Point(47, 217);
             this.SpeedLabel.Name = "SpeedLabel";
-            this.SpeedLabel.Size = new System.Drawing.Size(24, 13);
+            this.SpeedLabel.Size = new System.Drawing.Size(29, 12);
             this.SpeedLabel.TabIndex = 15;
             this.SpeedLabel.Text = "Idle";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 235);
+            this.label7.Location = new System.Drawing.Point(12, 217);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.Size = new System.Drawing.Size(35, 12);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Speed:";
+            this.label7.Text = "速度:";
             // 
             // FileLabel
             // 
             this.FileLabel.AutoSize = true;
-            this.FileLabel.Location = new System.Drawing.Point(59, 209);
+            this.FileLabel.Location = new System.Drawing.Point(47, 193);
             this.FileLabel.Name = "FileLabel";
-            this.FileLabel.Size = new System.Drawing.Size(24, 13);
+            this.FileLabel.Size = new System.Drawing.Size(29, 12);
             this.FileLabel.TabIndex = 17;
             this.FileLabel.Text = "Idle";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(27, 209);
+            this.label8.Location = new System.Drawing.Point(12, 193);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.Size = new System.Drawing.Size(35, 12);
             this.label8.TabIndex = 16;
-            this.label8.Text = "File:";
+            this.label8.Text = "文件:";
             // 
             // SizeLabel
             // 
             this.SizeLabel.AutoSize = true;
-            this.SizeLabel.Location = new System.Drawing.Point(59, 222);
+            this.SizeLabel.Location = new System.Drawing.Point(47, 205);
             this.SizeLabel.Name = "SizeLabel";
-            this.SizeLabel.Size = new System.Drawing.Size(24, 13);
+            this.SizeLabel.Size = new System.Drawing.Size(29, 12);
             this.SizeLabel.TabIndex = 19;
             this.SizeLabel.Text = "Idle";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(23, 222);
+            this.label10.Location = new System.Drawing.Point(12, 205);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(30, 13);
+            this.label10.Size = new System.Drawing.Size(35, 12);
             this.label10.TabIndex = 18;
-            this.label10.Text = "Size:";
+            this.label10.Text = "大小:";
             // 
             // ListButton
             // 
-            this.ListButton.Location = new System.Drawing.Point(278, 139);
+            this.ListButton.Location = new System.Drawing.Point(278, 128);
             this.ListButton.Name = "ListButton";
-            this.ListButton.Size = new System.Drawing.Size(75, 23);
+            this.ListButton.Size = new System.Drawing.Size(75, 21);
             this.ListButton.TabIndex = 20;
-            this.ListButton.Text = "Create List";
+            this.ListButton.Text = "创建列表";
             this.ListButton.UseVisualStyleBackColor = true;
             this.ListButton.Click += new System.EventHandler(this.ListButton_Click);
             // 
             // SourceLinkLabel
             // 
             this.SourceLinkLabel.AutoSize = true;
-            this.SourceLinkLabel.Location = new System.Drawing.Point(267, 266);
+            this.SourceLinkLabel.Location = new System.Drawing.Point(247, 247);
             this.SourceLinkLabel.Name = "SourceLinkLabel";
-            this.SourceLinkLabel.Size = new System.Drawing.Size(117, 13);
+            this.SourceLinkLabel.Size = new System.Drawing.Size(137, 12);
             this.SourceLinkLabel.TabIndex = 21;
             this.SourceLinkLabel.TabStop = true;
             this.SourceLinkLabel.Text = "Created by Jamie/Hello";
@@ -236,18 +236,18 @@
             // AllowCleanCheckBox
             // 
             this.AllowCleanCheckBox.AutoSize = true;
-            this.AllowCleanCheckBox.Location = new System.Drawing.Point(99, 116);
+            this.AllowCleanCheckBox.Location = new System.Drawing.Point(99, 109);
             this.AllowCleanCheckBox.Name = "AllowCleanCheckBox";
-            this.AllowCleanCheckBox.Size = new System.Drawing.Size(98, 17);
+            this.AllowCleanCheckBox.Size = new System.Drawing.Size(84, 16);
             this.AllowCleanCheckBox.TabIndex = 22;
-            this.AllowCleanCheckBox.Text = "Allow Clean Up";
+            this.AllowCleanCheckBox.Text = "清理服务器";
             this.AllowCleanCheckBox.UseVisualStyleBackColor = true;
             // 
             // AMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 290);
+            this.ClientSize = new System.Drawing.Size(396, 268);
             this.Controls.Add(this.AllowCleanCheckBox);
             this.Controls.Add(this.SourceLinkLabel);
             this.Controls.Add(this.ListButton);
