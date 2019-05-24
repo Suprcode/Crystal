6cdf5cf3b6fab2c1e6cd06ec48df137619a0b8de
suixin_pc <591821945@qq.com>
2019-05-24 21:36:17 +0800

    HttpServer add tow route :/regist and /addNameList

--- Server/Utils/HttpServer.cs
+++ Server/Utils/HttpServer.cs
@@ -1,11 +1,9 @@
-﻿using Server.MirEnvir;
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
-using System.Windows;
 
 namespace Server
 {
@@ -42,21 +40,59 @@ namespace Server
                     case "/":
                         writeRresponse(response, GameLanguage.GameName);
                         break;
-                    case "/login":
+                    case "/regist":
                         string id = request.QueryString["id"].ToString();
                         string psd = request.QueryString["psd"].ToString();
-                        int result = SMain.Envir.HTTPLogin(id, psd);
+                        string email = request.QueryString["email"].ToString();
+                        string name = request.QueryString["name"].ToString();
+                        string question = request.QueryString["question"].ToString();
+                        string answer = request.QueryString["answer"].ToString();
+                        string ip = request.QueryString["ip"].ToString();
+                        ClientPackets.NewAccount p = new ClientPackets.NewAccount();
+                        p.AccountID = id;
+                        p.Password = psd;
+                        p.EMailAddress = email;
+                        p.UserName = name;
+                        p.SecretQuestion = question;
+                        p.SecretAnswer = answer;
+                        int result = SMain.Envir.HTTPNewAccount(p,ip);
+                        writeRresponse(response, result.ToString());
+                        break;
+                    case "/login":
+                        id = request.QueryString["id"].ToString();
+                        psd = request.QueryString["psd"].ToString();
+                        result = SMain.Envir.HTTPLogin(id, psd);
                         writeRresponse(response, result.ToString());                        
+                        break;
+                    case "/addNameList":
+                        id = request.QueryString["id"].ToString();
+                        string fileName = request.QueryString["fileName"].ToString();                   
+                        addNameList(id, fileName);
+                        writeRresponse(response, "true");
                         break;
                     default:
                         writeRresponse(response, "error");
                         break;
                 }
             }
-            catch
+            catch (Exception error)
             {
-                writeRresponse(response, "error");
-            }            
+                writeRresponse(response, "error" + error);
+            }
+        }
+
+        void addNameList(string playerName,string fileName) {
+            fileName = Settings.NameListPath + fileName;
+            string sDirectory = Path.GetDirectoryName(fileName);
+            Directory.CreateDirectory(sDirectory);
+            string tempString = fileName;
+            if (File.ReadAllLines(tempString).All(t => playerName != t))
+            {
+                using (var line = File.AppendText(tempString))
+                {
+                    line.WriteLine(playerName);
+                }
+            }
         }
 
 
