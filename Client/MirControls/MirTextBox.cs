6e8983582e1f2c741019b386bee34d4a1a984a27
Raine <raine1988@hotmail.co.uk>
2020-06-20 08:26:16 +0100

    FullScreen Text fix

--- Client/MirControls/MirTextBox.cs
+++ Client/MirControls/MirTextBox.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Client.MirGraphics;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -47,6 +53,9 @@ namespace Client.MirControls
             base.OnLocationChanged();
             if (TextBox != null && !TextBox.IsDisposed)
                 TextBox.Location = DisplayLocation;
+
+            TextureValid = false;
+            Redraw();
         }
 
         #endregion
@@ -102,7 +111,7 @@ namespace Client.MirControls
 
         #region Font
 
-        public Font Font
+        public System.Drawing.Font Font
         {
             get
             {
@@ -124,21 +133,25 @@ namespace Client.MirControls
         protected override void OnSizeChanged()
         {
             TextBox.Size = Size;
-            _size = TextBox.Size;
+
+            DisposeTexture();
+
+            _size = Size;
 
             if (TextBox != null && !TextBox.IsDisposed)
                 base.OnSizeChanged();
         }
 
         #endregion
-        
+
         #region TextBox
 
         public bool CanLoseFocus;
         public readonly TextBox TextBox;
+        private Pen CaretPen;
 
         #endregion
-         
+
         #region Label
 
         public string Text
@@ -202,6 +215,7 @@ namespace Client.MirControls
                 if (Program.Form.ActiveControl == null || Program.Form.ActiveControl == Program.Form)
                     Program.Form.ActiveControl = TextBox;
 
+
             if (!TextBox.Visible)
                 if (Program.Form.ActiveControl == TextBox)
                     Program.Form.Focus();
@@ -216,8 +230,6 @@ namespace Client.MirControls
             if (TextBox.CanFocus) TextBox.Focus();
             else if (TextBox.Visible && TextBox.Parent != null)
                 Program.Form.ActiveControl = TextBox;
-
-
         }
 
         #endregion
@@ -227,7 +239,10 @@ namespace Client.MirControls
         public override void MultiLine()
         {
             TextBox.Multiline = true;
-            
+            TextBox.Size = Size;
+
+            DisposeTexture();
+            Redraw();
         }
 
         #endregion
@@ -236,25 +251,98 @@ namespace Client.MirControls
         {
             BackColour = Color.Black;
 
+            DrawControlTexture = true;
+            TextureValid = false;
+
             TextBox = new TextBox
             {
                 BackColor = BackColour,
                 BorderStyle = BorderStyle.None,
-                Font = new Font(Settings.FontName, 10F),
+                Font = new System.Drawing.Font(Settings.FontName, 10F * 96f / CMain.Graphics.DpiX),
                 ForeColor = ForeColour,
                 Location = DisplayLocation,
                 Size = Size,
                 Visible = Visible,
                 Tag = this,
+
             };
+
+            CaretPen = new Pen(ForeColour, 1);
 
             TextBox.VisibleChanged += TextBox_VisibleChanged;
             TextBox.ParentChanged += TextBox_VisibleChanged;
-            TextBox.KeyUp += TextBoxOnKeyUp;  
+            TextBox.KeyUp += TextBoxOnKeyUp;
             TextBox.KeyPress += TextBox_KeyPress;
+
+            TextBox.KeyPress += TextBox_NeedRedraw;
+            TextBox.KeyUp += TextBox_NeedRedraw;
+            TextBox.MouseDown += TextBox_NeedRedraw;
+            TextBox.MouseUp += TextBox_NeedRedraw;
+            TextBox.LostFocus += TextBox_NeedRedraw;
+            TextBox.GotFocus += TextBox_NeedRedraw;
+            TextBox.MouseWheel += TextBox_NeedRedraw;
 
             Shown += MirTextBox_Shown;
             TextBox.MouseMove += CMain.CMain_MouseMove;
+        }
+
+        private void TextBox_NeedRedraw(object sender, EventArgs e)
+        {
+            TextureValid = false;
+            Redraw();
+        }
+
+        protected unsafe override void CreateTexture()
+        {
+            if (!Settings.FullScreen) return;
+
+            if (Size.Width == 0 || Size.Height == 0)
+                return;
+
+            if (ControlTexture != null && !ControlTexture.Disposed && TextureSize != Size)
+                ControlTexture.Dispose();
+
+            if (ControlTexture == null || ControlTexture.Disposed)
+            {
+                DXManager.ControlList.Add(this);
+
+                ControlTexture = new Texture(DXManager.Device, Size.Width, Size.Height, 1, Usage.None, Format.A8R8G8B8, Pool.Managed);
+                ControlTexture.Disposing += ControlTexture_Disposing;
+                TextureSize = Size;
+            }
+
+            Point caret = GetCaretPosition();
+
+            using (GraphicsStream stream = ControlTexture.LockRectangle(0, LockFlags.Discard))
+            using (Bitmap bm = new Bitmap(Size.Width, Size.Height, Size.Width * 4, PixelFormat.Format32bppArgb, (IntPtr)stream.InternalDataPointer))
+            {
+                TextBox.DrawToBitmap(bm, new Rectangle(0, 0, Size.Width, Size.Height));
+                using (Graphics graphics = Graphics.FromImage(bm))
+                {
+                    graphics.DrawImage(bm, Point.Empty);
+                    if (TextBox.Focused)
+                        graphics.DrawLine(CaretPen, new Point(caret.X, caret.Y), new Point(caret.X, caret.Y + TextBox.Font.Height));
+                }
+
+            }
+            ControlTexture.UnlockRectangle(0);
+            DXManager.Sprite.Flush();
+            TextureValid = true;
+        }
+
+        private Point GetCaretPosition()
+        {
+            Point result = TextBox.GetPositionFromCharIndex(TextBox.SelectionStart);
+
+            if (result.X == 0 && TextBox.Text.Length > 0)
+            {
+                result = TextBox.GetPositionFromCharIndex(TextBox.Text.Length - 1);
+                int s = result.X / TextBox.Text.Length;
+                result.X = (int)(result.X + (s * 1.46));
+                result.Y = TextBox.GetLineFromCharIndex(TextBox.SelectionStart) * TextBox.Font.Height;
+            }
+
+            return result;
         }
 
         private void TextBoxOnKeyUp(object sender, KeyEventArgs e)
@@ -264,7 +352,6 @@ namespace Client.MirControls
                 case Keys.PrintScreen:
                     CMain.CMain_KeyUp(sender, e);
                     break;
-
             }
         }
 
@@ -287,6 +374,9 @@ namespace Client.MirControls
             CMain.Shift = false;
             CMain.Alt = false;
             CMain.Tilde = false;
+
+            TextureValid = false;
+            SetFocus();
         }
 
         public void SetFocus()
@@ -307,16 +397,16 @@ namespace Client.MirControls
 
             if (MirScene.ActiveScene != null && MirScene.ActiveScene.Controls.Count > 0)
             {
-                box1 = (MirMessageBox) MirScene.ActiveScene.Controls.FirstOrDefault(ob => ob is MirMessageBox);
-                box2 = (MirInputBox) MirScene.ActiveScene.Controls.FirstOrDefault(O => O is MirInputBox);
-                box3 = (MirAmountBox) MirScene.ActiveScene.Controls.FirstOrDefault(ob => ob is MirAmountBox);
+                box1 = (MirMessageBox)MirScene.ActiveScene.Controls.FirstOrDefault(ob => ob is MirMessageBox);
+                box2 = (MirInputBox)MirScene.ActiveScene.Controls.FirstOrDefault(O => O is MirInputBox);
+                box3 = (MirAmountBox)MirScene.ActiveScene.Controls.FirstOrDefault(ob => ob is MirAmountBox);
             }
 
-
-            if ((box1 != null && box1 != Parent) || (box2 != null && box2 != Parent)  || (box3 != null && box3 != Parent))
+            if ((box1 != null && box1 != Parent) || (box2 != null && box2 != Parent) || (box3 != null && box3 != Parent))
                 TextBox.Visible = false;
             else
                 TextBox.Visible = Visible && TextBox.Parent != null;
+
         }
 
 
