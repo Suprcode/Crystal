b42dcf5f48db044d0b54724b5c47f904e8610c76
DrowninGCode <chainedtorment@gmail.com>
2021-05-03 03:21:22 +0100

    Mob AI 16:
    Added:
    * Mandrill - Spell Animation (Teleport AI to be coded soon).
    * PlagueCrab - AI and spell animations
    * CreeperPlant - Frames added to Lib + AI and spell animations
    * FloatingWraith - Frames added to Lib + spell animations

---
+++ Server/MirObjects/Monsters/PlagueCrab.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+
+namespace Server.MirObjects.Monsters
+{
+    public class PlagueCrab : MonsterObject
+    {
+        protected internal PlagueCrab(MonsterInfo info)
+            : base(info)
+        {
+        }
+        protected override bool InAttackRange()
+        {
+            if (Target.CurrentMap != CurrentMap) return false;
+            if (Target.CurrentLocation == CurrentLocation) return false;
+
+            int x = Math.Abs(Target.CurrentLocation.X - CurrentLocation.X);
+            int y = Math.Abs(Target.CurrentLocation.Y - CurrentLocation.Y);
+
+            if (x > 4 || y > 2) return false;
+
+
+            return (x <= 4 && y <= 1) || (x == y || x % 2 == y % 2);
+        }
+
+        protected override void Attack()
+        {
+
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
+            Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 0 });
+            LineAttack(4);
+
+            ActionTime = Envir.Time + 300;
+            AttackTime = Envir.Time + AttackSpeed + 500;
+            ShockTime = 0;
+        }
+
+        private void LineAttack(int distance)
+        {
+            int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+            if (damage == 0) return;
+
+            for (int i = 1; i <= distance; i++)
+            {
+                Point target = Functions.PointMove(CurrentLocation, Direction, i);
+
+                if (target == Target.CurrentLocation)
+                {
+                    Target.Attacked(this, damage, DefenceType.ACAgility);
+                }
+                else
+                {
+                    if (!CurrentMap.ValidPoint(target)) continue;
+
+                    Cell cell = CurrentMap.GetCell(target);
+                    if (cell.Objects == null) continue;
+
+                    for (int o = 0; o < cell.Objects.Count; o++)
+                    {
+                        MapObject ob = cell.Objects[o];
+                        if (ob.Race == ObjectType.Monster || ob.Race == ObjectType.Player)
+                        {
+                            if (!ob.IsAttackTarget(this)) continue;
+
+                            ob.Attacked(this, damage, DefenceType.ACAgility);
+                        }
+                        else continue;
+
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
