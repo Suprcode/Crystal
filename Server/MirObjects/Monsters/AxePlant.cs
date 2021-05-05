edf30bea7adab963b177090aab148f576d41c287
DrowninGCode <chainedtorment@gmail.com>
2021-05-05 23:20:54 +0100

    Mob AI 17
    Added:
    * ArmedPlant - Frames to Lib File (326) + Spell animation
    * AvengerPlant - Frames to Lib File (327) + Spell animations
    * Nadz - Frames to Lib File (328) + AI and Spell animations
    * AvengingSpirit - Frames to Lib File (329) + AI and Spell animations
    * AvengingSpirit - Frames to Lib File (330) + AI and Spell animations
    * AxePlant - AI and Spell animation (Amended Die frameset  to Lib File (331))

---
+++ Server/MirObjects/Monsters/AxePlant.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+
+namespace Server.MirObjects.Monsters
+{
+    public class AxePlant : MonsterObject
+    {
+        protected internal AxePlant(MonsterInfo info)
+            : base(info)
+        {
+        }
+
+        protected override bool InAttackRange()
+        {
+            if (Target.CurrentMap != CurrentMap) return false;
+            if (Target.CurrentLocation == CurrentLocation) return false;
+
+            int x = Math.Abs(Target.CurrentLocation.X - CurrentLocation.X);
+            int y = Math.Abs(Target.CurrentLocation.Y - CurrentLocation.Y);
+
+            if (x > 2 || y > 2) return false;
+
+
+            return (x <= 1 && y <= 1) || (x == y || x % 2 == y % 2);
+        }
+
+        protected override void Attack()
+        {
+
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
+            bool range = !Functions.InRange(CurrentLocation, Target.CurrentLocation, 1);
+
+            ActionTime = Envir.Time + 500;
+            AttackTime = Envir.Time + AttackSpeed;
+            ShockTime = 0;
+
+            Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation });
+            LineAttack(2);
+        }
+
+        private void LineAttack(int distance)
+        {
+
+            int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+            if (damage == 0) return;
+
+            for (int i = 1; i <= distance; i++)
+            {
+                Point target = Functions.PointMove(CurrentLocation, Direction, i);
+
+                if (target == Target.CurrentLocation)
+                {
+
+                    int delay = Functions.MaxDistance(CurrentLocation, Target.CurrentLocation) * 50 + 700; //50 MS per Step
+                    DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + delay, Target, damage, DefenceType.ACAgility);
+                    ActionList.Add(action);
+                }
+                else
+                {
+                    if (!CurrentMap.ValidPoint(target)) continue;
+
+                    Cell cell = CurrentMap.GetCell(target);
+                    if (cell.Objects == null) continue;
+
+                    for (int o = 0; o < cell.Objects.Count; o++)
+                    {
+                        MapObject ob = cell.Objects[o];
+                        if (ob.Race == ObjectType.Monster || ob.Race == ObjectType.Player)
+                        {
+                            if (!ob.IsAttackTarget(this)) continue;
+
+                            int delay = Functions.MaxDistance(CurrentLocation, Target.CurrentLocation) * 50 + 700; //50 MS per Step
+                            DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + delay, ob, damage, DefenceType.ACAgility);
+                            ActionList.Add(action);                            
+                        }
+                        else continue;
+
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
