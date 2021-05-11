3c542d309b620b061def4182cb2bdb4e2cf24fda
DrowninGCode <chainedtorment@gmail.com>
2021-05-11 22:23:13 +0100

    Mob AI 19
    Amended:
    FrozenGolem - Amended Dead Frames (Lib 367)
    IcePhantom - Amended Dead Frames (Lib 368)
    BlackTortoise- Amended Dead Frames (Lib 372)
    
    Rewrote the level requirements for any push attacks for a few mob AIs.
    
    Added:
    FrozenGolem - Added spell animation (Basic Attack1 only mob)
    IcePhantom - Added AI and spell animation
    SnowWolf - Added AI and spell animation
    BlackTortoise - Added AI and spell animation
    DragonWarrior - Added AI and spell animation

---
+++ Server/MirObjects/Monsters/DragonWarrior.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+using System.Linq;
+using System.Text;
+
+namespace Server.MirObjects.Monsters
+{
+    class DragonWarrior : MonsterObject
+    {
+        protected internal DragonWarrior(MonsterInfo info)
+            : base(info)
+        {
+        }
+
+        protected override void Attack()
+        {
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            ActionTime = Envir.Time + 300;
+            AttackTime = Envir.Time + AttackSpeed;
+            ShockTime = 0;
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
+            if (Envir.Random.Next(5) > 0)
+            {
+                if (Envir.Random.Next(3) > 0)
+                {
+                    Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation });
+                    Attack1(); //Normal Attack         
+                }
+                else
+                {
+                    Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 1 });
+                    Attack2();//Spin Attack
+                }
+            }
+            else
+            {
+                Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 2 });
+                Attack3();//Shield Bash Attack
+            }
+
+            if (Target.Dead)
+                FindTarget();
+        }
+
+        private void Attack1()
+        {
+            int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+            if (damage == 0) return;
+            Target.Attacked(this, damage, DefenceType.ACAgility);
+        }
+
+        private void Attack2()
+        {
+            List<MapObject> targets = FindAllTargets(1, CurrentLocation);
+            if (targets.Count == 0) return;
+
+            for (int i = 0; i < targets.Count; i++)
+            {
+                int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+                if (damage == 0) return;
+                Target.Attacked(this, damage, DefenceType.ACAgility);
+            }
+        }
+
+        private void Attack3()
+        {
+            PushAttack(1);
+
+            if (Envir.Random.Next(Settings.PoisonResistWeight) >= Target.Stats[Stat.PoisonResist])
+            {
+                if (Envir.Random.Next(5) == 0)
+                    Target.ApplyPoison(new Poison { Owner = this, Duration = 5, PType = PoisonType.Paralysis, Value = GetAttackPower(Stats[Stat.MinSC], Stats[Stat.MaxSC]), TickSpeed = 2000 }, this);
+            }
+        }
+
+        private void PushAttack(int distance)
+        {
+            //need to put Damage Stats (DC/MC/SC) on mob for it to push
+            int levelGap = 5;
+            int mobLevel = this.Level;
+            int targetLevel = Target.Level;
+
+            if ((targetLevel <= mobLevel + levelGap))
+            {
+                if (Target.Pushed(this, Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation), 2) > 0)
+                {
+                    int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC] * 2);
+                    if (damage == 0) return;
+                    Target.Attacked(this, damage, DefenceType.ACAgility);
+                }
+                else
+                {                    
+                    Attack1(); //Normal Attack   
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Server/MirObjects/Monsters/SnowWolf.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+using System.Collections.Generic;
+
+namespace Server.MirObjects.Monsters
+{
+    public class SnowWolf : MonsterObject
+    {
+        protected internal SnowWolf(MonsterInfo info)
+            : base(info)
+        {
+        }
+
+        protected override void Attack()
+        {
+
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            ShockTime = 0;
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
+            ActionTime = Envir.Time + 300;
+            AttackTime = Envir.Time + AttackSpeed;
+
+            if (Envir.Random.Next(5) > 0)
+            {
+                Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation });
+                int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+                if (damage == 0) return;
+
+                DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + 350, Target, damage, DefenceType.ACAgility);
+                ActionList.Add(action);
+            }
+            else
+            {
+                Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 1 });
+                LineAttack(1);
+            }
+
+            if (Target.Dead)
+                FindTarget();
+
+        }
+
+        private void LineAttack(int distance)
+        {
+            List<MapObject> targets = FindAllTargets(distance, CurrentLocation);
+            if (targets.Count == 0) return;
+
+            for (int i = 0; i < targets.Count; i++)
+            {
+                AttackTime = Envir.Time + AttackSpeed + 300;
+                int damage = GetAttackPower(Stats[Stat.MinMC], Stats[Stat.MaxMAC]);
+                if (damage == 0) return;
+
+                DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + 450, Target, damage, DefenceType.ACAgility);
+                ActionList.Add(action);
+
+                if (Envir.Random.Next(Settings.PoisonResistWeight) >= Target.Stats[Stat.PoisonResist])
+                {
+                    if (Envir.Random.Next(4) == 0)
+                        Target.ApplyPoison(new Poison { Owner = this, Duration = 5, PType = PoisonType.Slow, Value = GetAttackPower(Stats[Stat.MinSC], Stats[Stat.MaxSC]), TickSpeed = 1000 }, this);
+                    if (Envir.Random.Next(8) == 0)
+                        Target.ApplyPoison(new Poison { Owner = this, Duration = 5, PType = PoisonType.Frozen, Value = GetAttackPower(Stats[Stat.MinSC], Stats[Stat.MaxSC]), TickSpeed = 1000 }, this);
+                }
+            }
+
+        }
+    }
+}
