d123ccf439710139ec267bfea70a315fa93ab360
DrowninGCode <chainedtorment@gmail.com>
2021-05-20 13:59:59 +0100

    Mob AI 24:
    Amended:
    * ScalyBeast (345.lib) - Removed blank frames from attacks (Duplicated one set of attack2 frames as only 7 pairs of frames were present).
    
    Added:
    * ColdArcher - AI and spell animations
    * FloatingRock - AI and spell animations
    * HornedArcher - AI and spell animations (Buffs to be added to AI).
    * ScalyBeast - AI and spell animations
    
    Notes: ColdArcher Arrow frames incomplete (arrow shooting wonky) - set as HornedArcher arrow temporarily? (Need to check WeMade Files as I suspect there may have been blanks which were removed).

---
+++ Server/MirObjects/Monsters/HornedArcher.cs
@@ -0,0 +1,126 @@
+﻿using Server.MirDatabase;
+using S = ServerPackets;
+
+namespace Server.MirObjects.Monsters
+{
+    // TODO - 3 BUFFS TO CODE
+    // Can buff all mobs around it
+    // Look at Yin/Yan Devil Nodes from Past Bichon?
+    // Add their spell effects to SpellObjects.cs
+    // Loop the animation while the buff is active.
+
+    public class HornedArcher : MonsterObject
+    {
+        public long FearTime;
+        public byte AttackRange = 8;
+
+        protected internal HornedArcher(MonsterInfo info)
+            : base(info)
+        {
+        }
+
+        protected override bool InAttackRange()
+        {
+            return CurrentMap == Target.CurrentMap && Functions.InRange(CurrentLocation, Target.CurrentLocation, AttackRange);
+        }
+
+        protected override void Attack()
+        {
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            ShockTime = 0;
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+            ActionTime = Envir.Time + 300;
+            AttackTime = Envir.Time + AttackSpeed;
+            int delay = Functions.MaxDistance(CurrentLocation, Target.CurrentLocation) * 50 + 500; //50 MS per Step
+
+            if (Envir.Random.Next(6) != 0)
+            {
+                Broadcast(new S.ObjectRangeAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, TargetID = Target.ObjectID });
+                int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+                if (damage == 0) return;
+
+                DelayedAction action = new DelayedAction(DelayedType.RangeDamage, Envir.Time + delay, Target, damage, DefenceType.ACAgility);
+                ActionList.Add(action);
+            }
+            else
+            {
+                Broadcast(new S.ObjectRangeAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, TargetID = Target.ObjectID, Type = 1 });
+                int damage = GetAttackPower(Stats[Stat.MinMC], Stats[Stat.MaxMC]);
+                if (damage == 0) return;
+
+                if (Envir.Random.Next(Settings.PoisonResistWeight) >= Target.Stats[Stat.PoisonResist])
+                {
+                    if (Envir.Random.Next(2) == 0)
+                    {
+                        Target.ApplyPoison(new Poison { Owner = this, Duration = 8, PType = PoisonType.Stun, Value = GetAttackPower(Stats[Stat.MinSC], Stats[Stat.MaxSC]), TickSpeed = 1000 }, this);
+                    }
+                }
+
+                DelayedAction action = new DelayedAction(DelayedType.RangeDamage, Envir.Time + delay, Target, damage, DefenceType.MACAgility);
+                ActionList.Add(action);
+            }
+
+            if (Target.Dead)
+                FindTarget();
+        }
+
+        protected override void ProcessTarget()
+        {
+            if (Target == null || !CanAttack) return;
+
+            if (InAttackRange() && Envir.Time < FearTime)
+            {
+                Attack();
+                return;
+            }
+
+            FearTime = Envir.Time + 5000;
+
+            if (Envir.Time < ShockTime)
+            {
+                Target = null;
+                return;
+            }
+
+            int dist = Functions.MaxDistance(CurrentLocation, Target.CurrentLocation);
+
+            if (dist >= AttackRange)
+                MoveTo(Target.CurrentLocation);
+            else
+            {
+                MirDirection dir = Functions.DirectionFromPoint(Target.CurrentLocation, CurrentLocation);
+
+                if (Walk(dir)) return;
+
+                switch (Envir.Random.Next(2)) //No favour
+                {
+                    case 0:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            dir = Functions.NextDir(dir);
+
+                            if (Walk(dir))
+                                return;
+                        }
+                        break;
+                    default:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            dir = Functions.PreviousDir(dir);
+
+                            if (Walk(dir))
+                                return;
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Server/MirObjects/Monsters/ScalyBeast.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using Server.MirDatabase;
+using Server.MirEnvir;
+using S = ServerPackets;
+using System.Collections.Generic;
+
+namespace Server.MirObjects.Monsters
+{
+    public class ScalyBeast : MonsterObject
+    {
+        protected internal ScalyBeast(MonsterInfo info)
+            : base(info)
+        {
+        }
+
+        protected override void Attack()
+        {
+            if (!Target.IsAttackTarget(this))
+            {
+                Target = null;
+                return;
+            }
+
+            ActionTime = Envir.Time + 300;
+            AttackTime = Envir.Time + AttackSpeed;
+            ShockTime = 0;
+
+            Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
+            if (Envir.Random.Next(3) > 0)
+            {
+                Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 0 });
+                int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+                if (damage == 0) return;
+
+                DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + 500, Target, damage, DefenceType.MAC);
+                ActionList.Add(action);
+            }
+            else
+            {
+                Broadcast(new S.ObjectAttack { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Type = 1 });
+                LineAttack();
+            }
+
+        }
+
+        private void AttackDetails()
+        {
+
+        }
+
+        private void LineAttack()
+        {
+            List<MapObject> targets = FindAllTargets(2, CurrentLocation);
+            if (targets.Count == 0) return;
+
+            for (int i = 0; i < targets.Count; i++)
+            {
+                int damage = GetAttackPower(Stats[Stat.MinDC], Stats[Stat.MaxDC]);
+                if (damage == 0) return;
+                DelayedAction action = new DelayedAction(DelayedType.Damage, Envir.Time + 900, targets[i], damage, DefenceType.MAC);
+                ActionList.Add(action);
+
+                if (Envir.Random.Next(Settings.PoisonResistWeight) >= Target.Stats[Stat.PoisonResist])
+                {
+                    if (Envir.Random.Next(5) == 0)
+                    {
+                        targets[i].ApplyPoison(new Poison { Owner = this, Duration = 4, PType = PoisonType.Paralysis, Value = GetAttackPower(Stats[Stat.MinSC], Stats[Stat.MaxSC]), TickSpeed = 1000 }, this);
+                    }
+                }
+            }
+
+        }
+    }
+}
