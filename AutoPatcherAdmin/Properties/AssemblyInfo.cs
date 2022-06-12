a91d2afa1cc47fa61b65d17300e0d385fee7a1b1
AndyF <development@suprcode.com>
2022-06-12 15:01:57 +0100

    Assembly version updates

--- AutoPatcherAdmin/Properties/AssemblyInfo.cs
+++ AutoPatcherAdmin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoPatcherAdmin")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.11.28.00")]
-[assembly: AssemblyFileVersion("2021.11.28.00")]
+[assembly: AssemblyVersion("2022.06.12.00")]
+[assembly: AssemblyFileVersion("2022.6.12.00")]
