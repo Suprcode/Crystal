410d999b50ce7187570532dd3b417157553976d8
Pete107 <pete107@gmail.com>
2022-07-25 19:33:03 +0100

    Fix for CHECKITEM

--- Server/MirObjects/NPC/Checks/CheckItem.cs
+++ Server/MirObjects/NPC/Checks/CheckItem.cs
@@ -3,25 +3,40 @@
 	[CheckCommand("CHECKITEM")]
 	public class CheckItem : NPCCheck
 	{
-		protected readonly ItemInfo RequiredItem;
+		protected ItemInfo RequiredItem;
+		protected readonly string ItemName;
 		protected readonly uint RequiredAmount;
 		protected readonly ushort RequiredDura = 0;
 		protected readonly bool RequiresDura = false;
 		public CheckItem(string line, string[] parts) : base(line, parts)
 		{
-			if (parts.Length < 3)
+			ItemName = parts[1];
+			if (parts.Length > 2)
+			{
+				if (!uint.TryParse(parts[2], out RequiredAmount))
+					return;
+			}
+			else
 				RequiredAmount = 1;
-			else if (!uint.TryParse(parts[2], out RequiredAmount))
-				return;
-			RequiresDura = parts.Length > 3 && ushort.TryParse(parts[3], out RequiredDura);
-			RequiredItem = Envir.GetItemInfo(parts[1]);
-			if (RequiredItem is null)
-				return;
+			if (parts.Length > 3)
+			{
+				if (!ushort.TryParse(parts[3], out RequiredDura))
+					return;
+				RequiresDura = true;
+			}
+			RequiredItem = Envir.GetItemInfo(ItemName);
 			InitializationSuccess = true;
 		}
 		public override bool Check(MapObject ob)
 		{
-			if (!InitializationSuccess) return false;
+			if (!InitializationSuccess) 
+				return false;
+			RequiredItem ??= Envir.GetItemInfo(ItemName);
+			if (RequiredItem is null)
+			{
+				InitializationSuccess = false;
+				return false;
+			}
 			switch (ob)
 			{
 				case PlayerObject player:
@@ -31,6 +46,8 @@
 					{
 						var playersItem = player.Info.Inventory[i];
 						if (playersItem is null)
+							continue;
+						if (playersItem.Info.Index != RequiredItem.Index)
 							continue;
 						if (RequiresDura &&
 							playersItem.CurrentDura < (RequiredDura * 1000))
