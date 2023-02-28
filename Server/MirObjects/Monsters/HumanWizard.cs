ad65cc20d8527059fe67334bd6bbc42526ac3395
PMySQLQ <86890440+PMySQLQ@users.noreply.github.com>
2023-02-28 20:32:36 +0800

    Mysterious Moon Forest Renewal Humanoid Monster
    Mysterious Moon Forest Renewal Humanoid Monster

--- Server/MirObjects/Monsters/HumanWizard.cs
+++ Server/MirObjects/Monsters/HumanWizard.cs
@@ -33,6 +33,7 @@ namespace Server.MirObjects.Monsters
             ShockTime = 0;
 
             Direction = Functions.DirectionFromPoint(CurrentLocation, Target.CurrentLocation);
+
             Broadcast(new S.ObjectMagic { ObjectID = ObjectID, Direction = Direction, Location = CurrentLocation, Spell = Spell.ThunderBolt, TargetID = Target.ObjectID, Target = Target.CurrentLocation, Cast = true, Level = 3 });
 
             ActionTime = Envir.Time + 300;
