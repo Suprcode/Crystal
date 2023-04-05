f6ddb3f4cfbec6d648d76c9d380440e9c9053820
OuYangPlane <106395089+OuYangPlane@users.noreply.github.com>
2023-04-05 10:18:33 +0800

    Rebirth effect add (1)
    file https://drive.google.com/file/d/104K0wroDyiwAgUjsxGq-AZQn7zieyAee/view?usp=sharing

--- Client/MirObjects/PlayerObject.cs
+++ Client/MirObjects/PlayerObject.cs
@@ -680,6 +680,48 @@ namespace Client.MirObjects
             {
                 Effects.Add(new SpecialEffect(Libraries.Effect, 296, 32, 3600, this, true, false, 1) { Repeat = true });
             }
+			if (LevelEffects.HasFlag(LevelEffects.Rebirth1))
+            {
+                Effects.Add(new SpecialEffect(Libraries.Effect, 1271, 36, 3600, this, true, false, 1) { Repeat = true,Delay=delay });
+               
+                
+            }
+
+            if (LevelEffects.HasFlag(LevelEffects.Rebirth2))
+            {
+                    Effects.Add(new SpecialEffect(Libraries.Effect, 1308, 38, 3600, this, true, false, 1) { Repeat = true });
+                SpecialEffect effect = new SpecialEffect(Libraries.Effect, 1347, 39, 3600, this, true, true, 1) { Repeat = true,Delay = delay };
+                effect.SetStart(CMain.Time + delay);
+                Effects.Add(effect);
+            }
+            if (LevelEffects.HasFlag(LevelEffects.Rebirth3))
+            {
+                Effects.Add(new SpecialEffect(Libraries.Effect, 1409, 19, 3600, this, true, false, 1) { Repeat = true });
+                SpecialEffect effect = new SpecialEffect(Libraries.Effect, 1429, 25, 4600, this, true, true, 1) { Repeat = true,Delay = delay };
+                effect.SetStart(CMain.Time + delay);
+                Effects.Add(effect);
+            }
+            if (LevelEffects.HasFlag(LevelEffects.NewBlue))
+            {
+                Effects.Add(new SpecialEffect(Libraries.Effect, 1574, 31, 3600, this, true, false, 1) { Repeat = true });
+                SpecialEffect effect = new SpecialEffect(Libraries.Effect, 1621, 31, 3600, this, true, false, 1) { Repeat = true,Delay = delay };
+                effect.SetStart(CMain.Time + delay);
+                Effects.Add(effect);
+            }
+            if (LevelEffects.HasFlag(LevelEffects.YellowDragon))
+            {
+                Effects.Add(new SpecialEffect(Libraries.Effect, 1646, 43, 4600, this, true, false, 1) { Repeat = true });
+                SpecialEffect effect = new SpecialEffect(Libraries.Effect, 1690, 36, 4600, this, true, false, 1) { Repeat = true,Delay = delay };
+                effect.SetStart(CMain.Time + delay);
+                Effects.Add(effect);
+            }
+            if (LevelEffects.HasFlag(LevelEffects.Phoenix))
+            {
+                Effects.Add(new SpecialEffect(Libraries.Effect, 1663, 26, 4600, this, true, false, 1) { Repeat = true });
+                SpecialEffect effect = new SpecialEffect(Libraries.Effect, 1705, 26, 4600, this, true, true, 1) { Repeat = true,Delay = delay };
+                effect.SetStart(CMain.Time + delay);
+                Effects.Add(effect);
+            }
         }
 
         public override void Process()
