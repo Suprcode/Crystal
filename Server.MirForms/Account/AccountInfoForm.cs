538e04993cdc7348fade28ffe680c06be84e1186
Jev <68875342+JevLOMCN@users.noreply.github.com>
2024-03-30 16:34:28 +0000

    Filter IP (#940)
    * Filter IP
    
    * PR FIX

--- Server.MirForms/Account/AccountInfoForm.cs
+++ Server.MirForms/Account/AccountInfoForm.cs
@@ -112,6 +112,9 @@ namespace Server
             else if (FilterPlayerTextBox.Text.Length > 0)
                 accounts = SMain.Envir.MatchAccountsByPlayer(FilterPlayerTextBox.Text, MatchFilterCheckBox.Checked);
 
+            else if (FilterIPTextBox.Text.Length > 0)
+                accounts = SMain.Envir.MatchAccountsByIP(FilterIPTextBox.Text, MatchFilterCheckBox.Checked);
+
             if (AccountInfoListView.Items.Count != accounts.Count)
             {
                 AccountInfoListView.SelectedIndexChanged -= AccountInfoListView_SelectedIndexChanged;
