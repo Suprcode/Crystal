6922a3a3cff1d7c8b337f841c53c1c1c5671cba8
Jev <68875342+JevLOMCN@users.noreply.github.com>
2024-11-04 19:44:09 +0000

    Guild Item Form Improvements
    Added Buff Info, Guild Notice

--- Server.MirForms/Systems/GuildItemForm.Designer.cs
+++ Server.MirForms/Systems/GuildItemForm.Designer.cs
@@ -38,13 +38,30 @@
             Members = new ColumnHeader();
             Rank = new ColumnHeader();
             DeleteButton = new Button();
+            GuildNoticeBox = new RichTextBox();
+            MemberCountLabel = new Label();
+            BuffListView = new ListView();
+            BuffID = new ColumnHeader();
+            BuffName = new ColumnHeader();
+            BuffActivity = new ColumnHeader();
+            BuffTime = new ColumnHeader();
+            GuildNoticeGroupBox = new GroupBox();
+            GuildStorageGroupBox = new GroupBox();
+            GuildMembersGroupBox = new GroupBox();
+            GuildEXPLabel = new Label();
+            GuildBuffsGroupBox = new GroupBox();
+            GuildPointsLabel = new Label();
+            GuildNoticeGroupBox.SuspendLayout();
+            GuildStorageGroupBox.SuspendLayout();
+            GuildMembersGroupBox.SuspendLayout();
+            GuildBuffsGroupBox.SuspendLayout();
             SuspendLayout();
             // 
             // GuildItemListView
             // 
             GuildItemListView.Columns.AddRange(new ColumnHeader[] { indexHeader, PlaceHeader, nameHeader, countHeader, DuraHeader });
             GuildItemListView.GridLines = true;
-            GuildItemListView.Location = new Point(1, 0);
+            GuildItemListView.Location = new Point(10, 22);
             GuildItemListView.Name = "GuildItemListView";
             GuildItemListView.Size = new Size(520, 414);
             GuildItemListView.TabIndex = 0;
@@ -80,10 +97,10 @@
             // 
             MemberListView.Columns.AddRange(new ColumnHeader[] { Members, Rank });
             MemberListView.GridLines = true;
-            MemberListView.Location = new Point(547, 12);
+            MemberListView.Location = new Point(8, 22);
             MemberListView.Name = "MemberListView";
             MemberListView.Scrollable = false;
-            MemberListView.Size = new Size(291, 323);
+            MemberListView.Size = new Size(291, 369);
             MemberListView.TabIndex = 1;
             MemberListView.UseCompatibleStateImageBehavior = false;
             MemberListView.View = View.Details;
@@ -100,7 +117,7 @@
             // 
             // DeleteButton
             // 
-            DeleteButton.Location = new Point(654, 356);
+            DeleteButton.Location = new Point(115, 397);
             DeleteButton.Name = "DeleteButton";
             DeleteButton.Size = new Size(75, 32);
             DeleteButton.TabIndex = 2;
@@ -108,16 +125,137 @@
             DeleteButton.UseVisualStyleBackColor = true;
             DeleteButton.Click += DeleteButton_Click;
             // 
+            // GuildNoticeBox
+            // 
+            GuildNoticeBox.BorderStyle = BorderStyle.None;
+            GuildNoticeBox.Location = new Point(8, 22);
+            GuildNoticeBox.Name = "GuildNoticeBox";
+            GuildNoticeBox.ReadOnly = true;
+            GuildNoticeBox.Size = new Size(399, 219);
+            GuildNoticeBox.TabIndex = 3;
+            GuildNoticeBox.Text = "";
+            // 
+            // MemberCountLabel
+            // 
+            MemberCountLabel.AutoSize = true;
+            MemberCountLabel.Location = new Point(6, 398);
+            MemberCountLabel.Name = "MemberCountLabel";
+            MemberCountLabel.Size = new Size(60, 15);
+            MemberCountLabel.TabIndex = 4;
+            MemberCountLabel.Text = "Members:";
+            // 
+            // BuffListView
+            // 
+            BuffListView.BorderStyle = BorderStyle.None;
+            BuffListView.Columns.AddRange(new ColumnHeader[] { BuffID, BuffName, BuffActivity, BuffTime });
+            BuffListView.FullRowSelect = true;
+            BuffListView.GridLines = true;
+            BuffListView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            BuffListView.Location = new Point(6, 40);
+            BuffListView.Name = "BuffListView";
+            BuffListView.Size = new Size(422, 201);
+            BuffListView.TabIndex = 5;
+            BuffListView.UseCompatibleStateImageBehavior = false;
+            BuffListView.View = View.Details;
+            // 
+            // BuffID
+            // 
+            BuffID.Text = "ID";
+            // 
+            // BuffName
+            // 
+            BuffName.Text = "Name";
+            BuffName.Width = 140;
+            // 
+            // BuffActivity
+            // 
+            BuffActivity.Text = "Active";
+            BuffActivity.Width = 80;
+            // 
+            // BuffTime
+            // 
+            BuffTime.Text = "Duration (Mins)";
+            BuffTime.Width = 135;
+            // 
+            // GuildNoticeGroupBox
+            // 
+            GuildNoticeGroupBox.Controls.Add(GuildNoticeBox);
+            GuildNoticeGroupBox.Location = new Point(442, 452);
+            GuildNoticeGroupBox.Name = "GuildNoticeGroupBox";
+            GuildNoticeGroupBox.Size = new Size(413, 249);
+            GuildNoticeGroupBox.TabIndex = 6;
+            GuildNoticeGroupBox.TabStop = false;
+            GuildNoticeGroupBox.Text = "Notice";
+            // 
+            // GuildStorageGroupBox
+            // 
+            GuildStorageGroupBox.Controls.Add(GuildItemListView);
+            GuildStorageGroupBox.Location = new Point(2, 0);
+            GuildStorageGroupBox.Name = "GuildStorageGroupBox";
+            GuildStorageGroupBox.Size = new Size(539, 446);
+            GuildStorageGroupBox.TabIndex = 7;
+            GuildStorageGroupBox.TabStop = false;
+            GuildStorageGroupBox.Text = "Storage";
+            // 
+            // GuildMembersGroupBox
+            // 
+            GuildMembersGroupBox.Controls.Add(GuildEXPLabel);
+            GuildMembersGroupBox.Controls.Add(MemberListView);
+            GuildMembersGroupBox.Controls.Add(DeleteButton);
+            GuildMembersGroupBox.Controls.Add(MemberCountLabel);
+            GuildMembersGroupBox.Location = new Point(547, 0);
+            GuildMembersGroupBox.Name = "GuildMembersGroupBox";
+            GuildMembersGroupBox.Size = new Size(308, 446);
+            GuildMembersGroupBox.TabIndex = 8;
+            GuildMembersGroupBox.TabStop = false;
+            GuildMembersGroupBox.Text = "Members/Ranks";
+            // 
+            // GuildEXPLabel
+            // 
+            GuildEXPLabel.AutoSize = true;
+            GuildEXPLabel.Location = new Point(8, 421);
+            GuildEXPLabel.Name = "GuildEXPLabel";
+            GuildEXPLabel.Size = new Size(30, 15);
+            GuildEXPLabel.TabIndex = 5;
+            GuildEXPLabel.Text = "EXP:";
+            // 
+            // GuildBuffsGroupBox
+            // 
+            GuildBuffsGroupBox.Controls.Add(GuildPointsLabel);
+            GuildBuffsGroupBox.Controls.Add(BuffListView);
+            GuildBuffsGroupBox.Location = new Point(2, 452);
+            GuildBuffsGroupBox.Name = "GuildBuffsGroupBox";
+            GuildBuffsGroupBox.Size = new Size(434, 249);
+            GuildBuffsGroupBox.TabIndex = 9;
+            GuildBuffsGroupBox.TabStop = false;
+            GuildBuffsGroupBox.Text = "Buffs";
+            // 
+            // GuildPointsLabel
+            // 
+            GuildPointsLabel.AutoSize = true;
+            GuildPointsLabel.Location = new Point(6, 19);
+            GuildPointsLabel.Name = "GuildPointsLabel";
+            GuildPointsLabel.Size = new Size(43, 15);
+            GuildPointsLabel.TabIndex = 6;
+            GuildPointsLabel.Text = "Points:";
+            // 
             // GuildItemForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(854, 450);
-            Controls.Add(DeleteButton);
-            Controls.Add(MemberListView);
-            Controls.Add(GuildItemListView);
+            ClientSize = new Size(865, 705);
+            Controls.Add(GuildBuffsGroupBox);
+            Controls.Add(GuildMembersGroupBox);
+            Controls.Add(GuildStorageGroupBox);
+            Controls.Add(GuildNoticeGroupBox);
             Name = "GuildItemForm";
             Text = "GuildItemForm";
+            GuildNoticeGroupBox.ResumeLayout(false);
+            GuildStorageGroupBox.ResumeLayout(false);
+            GuildMembersGroupBox.ResumeLayout(false);
+            GuildMembersGroupBox.PerformLayout();
+            GuildBuffsGroupBox.ResumeLayout(false);
+            GuildBuffsGroupBox.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -132,5 +270,18 @@
         private Button DeleteButton;
         public ListView GuildItemListView;
         public ListView MemberListView;
+        private RichTextBox GuildNoticeBox;
+        private Label MemberCountLabel;
+        private ListView BuffListView;
+        private ColumnHeader BuffID;
+        private ColumnHeader BuffName;
+        private ColumnHeader BuffActivity;
+        private ColumnHeader BuffTime;
+        private GroupBox GuildNoticeGroupBox;
+        private GroupBox GuildStorageGroupBox;
+        private GroupBox GuildMembersGroupBox;
+        private GroupBox GuildBuffsGroupBox;
+        private Label GuildPointsLabel;
+        private Label GuildEXPLabel;
     }
 }
--- Server.MirForms/Systems/GuildItemForm.cs
+++ Server.MirForms/Systems/GuildItemForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Server.MirObjects;
+using Server.MirObjects.Monsters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +21,68 @@ namespace Server.Systems
         {
             InitializeComponent();
         }
+
+        #region Load Guild Notice
+        public void SetGuildNotice(List<string> notice)
+        {
+            GuildNoticeBox.Text = string.Join(Environment.NewLine, notice);
+        }
+        #endregion
+
+        #region Load Member Count
+        public void SetMemberCount(int memberCount, int memberCap)
+        {
+            MemberCountLabel.Text = $"Members: {memberCount}/{memberCap}";
+        }
+        #endregion
+
+        #region Load Guild Points
+        public void SetGuildPoints(byte sparePoints)
+        {
+            GuildPointsLabel.Text = $"Points: {sparePoints}";
+        }
+        #endregion
+
+        #region Load Guild EXP
+        public void SetGuildExperience(long experience)
+        {
+            GuildEXPLabel.Text = $"EXP: {experience}";
+        }
+        #endregion
+
+        #region Buff List
+        public void SetBuffList(List<GuildBuff> activeBuffs, List<GuildBuffInfo> allBuffInfos)
+        {
+            BuffListView.Items.Clear();
+
+            // Dictionary to quickly check if a buff is active
+            var activeBuffsById = activeBuffs.ToDictionary(buff => buff.Id);
+
+            foreach (var buffInfo in allBuffInfos)
+            {
+                ListViewItem item = new ListViewItem(buffInfo.Id.ToString());
+
+                // Display the name of the buff
+                item.SubItems.Add(buffInfo.Name);
+
+                // Check if this buff is active
+                if (activeBuffsById.TryGetValue(buffInfo.Id, out GuildBuff activeBuff))
+                {
+                    // Buff is active
+                    item.SubItems.Add("Active");
+                    item.SubItems.Add(activeBuff.ActiveTimeRemaining.ToString());
+                }
+                else
+                {
+                    // Buff is inactive
+                    item.SubItems.Add("Inactive");
+                    item.SubItems.Add("0"); // No time remaining for inactive buff
+                }
+
+                BuffListView.Items.Add(item);
+            }
+        }
+        #endregion
 
         #region Delete Button
         private void DeleteButton_Click(object sender, EventArgs e)
