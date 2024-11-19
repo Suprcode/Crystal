613acc33e42bafc67b09f9b0b880559826dd38f5
Damian Day <damianday@users.noreply.github.com>
2024-11-19 11:09:18 +0000

    Compression Fixes #968 (#1017)
    Move CompressBytes/DecompressBytes to Functions.
    Add new packet CompressedPacket.
    Make use of compression with NPC goods, reduces packet size much!
    
    Simplify Compression
    
    Remove duplicate GetItemInfo
    
    Update Enums.cs

--- Server/MirObjects/PlayerObject.cs
+++ Server/MirObjects/PlayerObject.cs
@@ -13904,27 +13904,9 @@ namespace Server.MirObjects
             Enqueue(p);
         }
 
-        public void SendNPCGoods(S.NPCGoods goods)
+        public void SendNPCGoods(List<UserItem> goods, float rate, PanelType panelType, bool hideAddedStats = false)
         {
-            var chunks = Functions.SplitList(10, goods.List); // Split into chunks of 10..
-            if (chunks.Count == 1)
-            {
-                goods.Progress = 3;
-                Enqueue(goods);
-                return;
-            }
-
-            //  Loop through the chunks
-            for (var i = 0; i < chunks.Count; i++)
-            {
-                byte prog;
-
-                if (i == 0) prog = 1; // First List
-                else if (i == chunks.Count - 1) prog = 3; // Final List
-                else prog = 2; // Middle
-
-                Enqueue(new S.NPCGoods { Progress = prog, List = chunks[i], Rate = goods.Rate, Type = goods.Type, HideAddedStats = goods.HideAddedStats });
-            }
+            Enqueue(new S.NPCGoods { List = goods, Rate = rate, Type = panelType, HideAddedStats = hideAddedStats });
         }
     }
 }
--- Shared/ServerPackets.cs
+++ Shared/ServerPackets.cs
@@ -2992,8 +2992,8 @@ namespace ServerPackets
     public sealed class NPCGoods : Packet
     {
         public override short Index { get { return (short)ServerPacketIds.NPCGoods; } }
+        public override bool Compressed => true;
 
-        public byte Progress; // 1: Start, 2: Middle, 3: End
         public List<UserItem> List = new List<UserItem>();
         public float Rate;
         public PanelType Type;
@@ -3001,8 +3001,6 @@ namespace ServerPackets
 
         protected override void ReadPacket(BinaryReader reader)
         {
-            Progress = reader.ReadByte();
-
             int count = reader.ReadInt32();
             for (int i = 0; i < count; i++)
                 List.Add(new UserItem(reader));
@@ -3014,8 +3012,6 @@ namespace ServerPackets
         }
         protected override void WritePacket(BinaryWriter writer)
         {
-            writer.Write(Progress);
-
             writer.Write(List.Count);
             for (int i = 0; i < List.Count; i++)
                 List[i].Save(writer);
