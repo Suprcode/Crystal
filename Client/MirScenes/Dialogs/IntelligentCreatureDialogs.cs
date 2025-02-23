4c6418764fed42d09e58befe54fb57c9bd08d0c2
syf7211256 <170974686a@gmail.com>
2025-02-23 16:36:49 +0800

    Update IntelligentCreatureDialogs.cs
    fix creature name can not be chinese

--- Client/MirScenes/Dialogs/IntelligentCreatureDialogs.cs
+++ Client/MirScenes/Dialogs/IntelligentCreatureDialogs.cs
@@ -18,7 +18,7 @@ namespace Client.MirScenes.Dialogs
         public int SelectedCreatureSlot = -1;
         public MirControl HoverLabelParent = null;
 
-        private readonly Regex CreatureNameReg = new Regex(@"^[A-Za-z0-9]{" + Globals.MinCharacterNameLength + "," + Globals.MaxCharacterNameLength + "}$");
+        private readonly Regex CreatureNameReg = new Regex(@"^[\p{L}\p{N}]{" + Globals.MinCharacterNameLength + "," + Globals.MaxCharacterNameLength + "}$");
 
         private MirAnimatedControl CreatureImage;
         public long SwitchAnimTime;
