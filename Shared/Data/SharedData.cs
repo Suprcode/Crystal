5054362653e76bb0dc6d272aa2dc236e4889fac4
Jev <68875342+JevLOMCN@users.noreply.github.com>
2025-03-03 22:25:03 +0000

    Moved clientGTMap to SharedData
    Removed from Enum

--- Shared/Data/SharedData.cs
+++ Shared/Data/SharedData.cs
@@ -136,3 +136,43 @@ public class WorldMapIcon
         writer.Write(MapIndex);
     }
 }
+public class ClientGTMap
+{
+    public int index;
+    public string Name;
+    public string Owner;
+    public string Leader;
+    public string Leader2;
+    public int price;
+    public int days;
+    public int begin;
+
+    public ClientGTMap()
+    {
+
+    }
+
+    public ClientGTMap(BinaryReader reader)
+    {
+        index = reader.ReadInt32();
+        Name = reader.ReadString();
+        Owner = reader.ReadString();
+        Leader = reader.ReadString();
+        Leader2 = reader.ReadString();
+        price = reader.ReadInt32();
+        days = reader.ReadInt32();
+        begin = reader.ReadInt32();
+    }
+
+    public void Save(BinaryWriter writer)
+    {
+        writer.Write(index);
+        writer.Write(Name);
+        writer.Write(Owner);
+        writer.Write(Leader);
+        writer.Write(Leader2);
+        writer.Write(price);
+        writer.Write(days);
+        writer.Write(begin);
+    }
+}
