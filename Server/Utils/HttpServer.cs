042457b00f819a373ef2622260f1beb9fe335f8f
Jev <68875342+JevLOMCN@users.noreply.github.com>
2025-06-01 14:07:38 +0100

    More HTTP functionality

--- Server/Utils/HttpServer.cs
+++ Server/Utils/HttpServer.cs
@@ -1,14 +1,17 @@
 ﻿using System.Net;
+using System.Text;
+using System.Text.Json;
 using Server.MirEnvir;
 using S = ServerPackets;
+using Server.MirDatabase;
 
 namespace Server.Library.Utils
 {
     class HttpServer : HttpService
     {
+
         Thread _thread;
         CancellationTokenSource tokenSource = new();
-
         public HttpServer()
         {
             Host = Settings.HTTPIPAddress;
@@ -23,13 +26,28 @@ namespace Server.Library.Utils
         public new void Stop()
         {
             base.Stop();
-            
+
             tokenSource.Cancel();
             Thread.Sleep(1000);
             tokenSource.Dispose();
-
+        }
+        protected void WriteResponse(HttpListenerResponse response, string responseString)
+        {
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentType = "text/plain; charset=utf-8";
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
         }
 
+        protected void WriteJsonResponse(HttpListenerResponse response, string json)
+        {
+            var buffer = Encoding.UTF8.GetBytes(json);
+            response.ContentType = "application/json; charset=utf-8";
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+        }
 
         public override void OnGetRequest(HttpListenerRequest request, HttpListenerResponse response)
         {
@@ -39,6 +57,7 @@ namespace Server.Library.Utils
                 url = url.Substring(0, url.IndexOf("?", StringComparison.Ordinal));
                 url = url.ToLower();
             }
+
             try
             {
                 switch (url)
@@ -46,6 +65,88 @@ namespace Server.Library.Utils
                     case "/":
                         WriteResponse(response, GameLanguage.GameName);
                         break;
+
+                    case "/status":
+                        try
+                        {
+                            var isOnline = Envir.Main?.Running ?? false;
+                            WriteResponse(response, isOnline ? "Online" : "Offline");
+                        }
+                        catch (Exception ex)
+                        {
+                            WriteResponse(response, "It hung :(");
+                        }
+                        break;
+
+                    case "/usercount":
+                        try
+                        {
+                            var count = Envir.Main?.PlayerCount ?? 0;
+                            WriteResponse(response, count.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            WriteResponse(response, "It hung :(");
+                        }
+                        break;
+
+                    case "/listaccounts":
+                        {
+                            // Directly use Envir.Main.AccountList
+                            var rawList = Envir.Main?.AccountList;
+                            if (rawList == null)
+                            {
+                                Console.WriteLine("[HTTP] /listaccounts → AccountList is null");
+                                WriteJsonResponse(response, "[]");
+                                break;
+                            }
+
+                            Console.WriteLine($"[HTTP] /listaccounts → found {rawList.Count} account(s).");
+
+                            var accountDtos = rawList.Select(account => new
+                            {
+                                account.Index,
+                                account.AccountID,
+                                account.UserName,
+                                account.AdminAccount,
+                                account.Banned,
+                                account.BanReason,
+                                account.ExpiryDate,
+                                account.Gold,
+                                account.Credit
+                            }).ToList();
+
+                            string json = JsonSerializer.Serialize(accountDtos, new JsonSerializerOptions { WriteIndented = true });
+                            WriteJsonResponse(response, json);
+                        }
+                        break;
+
+                    //case "/playersonline":
+                    //    try
+                    //    {
+                    //        var playerNames = Envir.Main?.Players
+                    //            .Where(p => p?.Info != null)
+                    //            .Select(p => new
+                    //            {
+                    //                p.Info.Index,
+                    //                p.Info.Name,
+                    //                p.Info.Level,
+                    //                Class = p.Info.Class.ToString(),
+                    //                Gender = p.Info.Gender.ToString(),
+                    //                Map = p.CurrentMap?.Info?.Title
+                    //            });
+                    //        Console.WriteLine("[HTTP] Online Players: " + string.Join(", ", playerNames));
+
+                    //        var json = JsonSerializer.Serialize(playerNames);
+                    //        WriteJsonResponse(response, json);
+                    //    }
+                    //    catch (Exception ex)
+                    //    {
+                    //        Console.WriteLine("[HTTP] /playersonline error: " + ex);
+                    //        WriteJsonResponse(response, "[]");
+                    //    }
+                    //    break;
+
                     case "/newaccount":
                         var id = request.QueryString["id"];
                         var psd = request.QueryString["psd"];
@@ -54,22 +155,26 @@ namespace Server.Library.Utils
                         var question = request.QueryString["question"];
                         var answer = request.QueryString["answer"];
                         var ip = request.QueryString["ip"];
-                        var p = new ClientPackets.NewAccount();
-                        p.AccountID = id;
-                        p.Password = psd;
-                        p.EMailAddress = email;
-                        p.UserName = name;
-                        p.SecretQuestion = question;
-                        p.SecretAnswer = answer;
+                        var p = new ClientPackets.NewAccount
+                        {
+                            AccountID = id,
+                            Password = psd,
+                            EMailAddress = email,
+                            UserName = name,
+                            SecretQuestion = question,
+                            SecretAnswer = answer
+                        };
                         var result = Envir.Main.HTTPNewAccount(p, ip);
                         WriteResponse(response, result.ToString());
-                        break;                               
+                        break;
+
                     case "/addnamelist":
                         id = request.QueryString["id"];
                         var fileName = request.QueryString["fileName"];
                         AddNameList(id, fileName);
                         WriteResponse(response, "true");
-                        break;              
+                        break;
+
                     case "/broadcast":
                         var msg = request.QueryString["msg"];
                         if (msg.Length < 5)
@@ -84,6 +189,7 @@ namespace Server.Library.Utils
                         });
                         WriteResponse(response, "true");
                         break;
+
                     default:
                         WriteResponse(response, "error");
                         break;
@@ -93,13 +199,14 @@ namespace Server.Library.Utils
             {
                 WriteResponse(response, "request error: " + error);
             }
-        }      
+        }
 
         void AddNameList(string playerName, string fileName)
         {
             fileName = Path.Combine(Settings.NameListPath, fileName);
             var sDirectory = Path.GetDirectoryName(fileName);
             Directory.CreateDirectory(sDirectory ?? throw new InvalidOperationException());
+
             if (!File.Exists(fileName))
                 File.Create(fileName).Close();
 
@@ -111,12 +218,11 @@ namespace Server.Library.Utils
                     line.WriteLine(playerName);
                 }
             }
-        }    
+        }
 
         public override void OnPostRequest(HttpListenerRequest request, HttpListenerResponse response)
         {
             Console.WriteLine("POST request: {0}", request.Url);
         }
     }
-
 }
